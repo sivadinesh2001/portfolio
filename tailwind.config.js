360414f4dd1ed8c56b3e638b7e360eda93c64d9c
Sivadinesh2001 <sivadineshkanakamalla@gmail.com>
2025-10-25 13:08:57 +0530

    Initial portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'media', // or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
